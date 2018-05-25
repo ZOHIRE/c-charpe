a56d8d2d462a44ead06f977dd6ca6803c6d8f98e
walid <walid@WALID-PC>
2018-05-26 01:41:42 +0200

    add comment

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -10,6 +10,8 @@ namespace FirstProgram
     {
         static void Main(string[] args)
         {
+            //This is my ferst program
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
         }
